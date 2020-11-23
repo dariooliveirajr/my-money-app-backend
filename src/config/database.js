86738f827b695cc2705da930d98edfeaca1dd8c7
Dário Oliveira Júnior <juniordario06@gmail.com>
2020-11-22 21:28:02 -0300

    att

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb.dariooliveirajr.com', { useNewUrlParser: true })
+module.exports = mongoose.connect('mongo71-farm76.kinghost.net', { useNewUrlParser: true })
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório"
 mongoose.Error.messages.Number.min =
